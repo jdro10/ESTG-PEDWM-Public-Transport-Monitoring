5fb3e1528f23215e466d7de37195144932967276
xPromate <jpcm_1998@hotmail.com>
2021-05-11 22:14:34 +0100

    added aop support for controllers

---
+++ src/main/java/estg/publictransportmonitoring/aop/LoggingAspect.java
@@ -0,0 +1,40 @@
+package estg.publictransportmonitoring.aop;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+@Aspect
+@Component
+public class LoggingAspect
+{
+    private static final Logger LOGGER = LogManager.getLogger(LoggingAspect.class);
+
+    @Around("execution(* estg.publictransportmonitoring.Controllers..*(..)))")
+    public Object profileAllMethods(ProceedingJoinPoint proceedingJoinPoint) throws Throwable
+    {
+        MethodSignature methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
+
+        //Get intercepted method details
+        String className = methodSignature.getDeclaringType().getSimpleName();
+        String methodName = methodSignature.getName();
+
+        final StopWatch stopWatch = new StopWatch();
+
+        //Measure method execution time
+        stopWatch.start();
+        Object result = proceedingJoinPoint.proceed();
+        stopWatch.stop();
+
+        //Log method execution time
+        System.out.println("Execution time of " + className + "." + methodName + " "
+                + ":: " + stopWatch.getTotalTimeMillis() + " ms");
+
+        return result;
+    }
+}
