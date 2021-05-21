154a0ba6f6afcb2cd4ac9cf576f94e4e6aa2e067
xPromate <jpcm_1998@hotmail.com>
2021-05-21 22:02:45 +0100

    saved token in frontend

--- public-transport-monitoring-pwa/src/components/Login/Login.js
+++ public-transport-monitoring-pwa/src/components/Login/Login.js
@@ -1,22 +1,46 @@
 import './login.css'
-import React from 'react'
+import React , { useState } from 'react'
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
 
 const Login = () => {
+    const storedJwt = localStorage.getItem('token');
+    const [jwt ,setJwt] = useState(storedJwt || null);
+
+
+    const getJwt = async () => {
+        const res = await fetch('http://localhost:8080/login',{
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify({"username": "user02",
+            "password": "123"})
+        })
+
+        const test = await res.json()
+        console.log(test)
+        console.log('fiz pedido')
+        
+        localStorage.setItem('token', test.token);
+        
+        setJwt(test.token);
+        console.log(test.token)
+      };
+
     return (
         <div id="loginDiv" className="Login">
             <Form>
                 <Form.Group id="input" size="lg" controlId="email">
                 <Form.Label>Email</Form.Label>
-                <Form.Control type="email"/>
+                <Form.Control/>
                 </Form.Group>
                 <Form.Group id="input" size="lg" controlId="password">
                 <Form.Label>Password</Form.Label>
                 <Form.Control type="password"/>
                 </Form.Group>
-                <Button variant="success" block size="lg" type="submit"> Login </Button>
+                <Button onClick={() => getJwt()} variant="success" block size="lg" > Login </Button>
                 <div id="signupMsg">
                     <Form.Label>Não tem uma conta? <a href="/">Registe-se aqui.</a></Form.Label> {/*mudar para Link depois */}
                 </div>
@@ -24,5 +48,7 @@ const Login = () => {
         </div>
     )
 }
+
+
 
 export default Login
